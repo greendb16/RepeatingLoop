11007f9aa6fab5fd9e4cb41fbfaaf6b3ac344020
greendb16 <greendb16@gmail.com>
2019-06-13 17:04:34 -0400

    First commit

---
+++ src/RepeatingLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RepeatingLoop {
+    static public void main(String[]args) {
+        Scanner key = new Scanner(System.in);
+        int secretNumber, guess = 0;
+
+        secretNumber = 123;
+        System.out.println("I'm thinking of a number between 1 and 1000");
+        System.out.println("Enter the number:  ");
+        guess = key.nextInt();
+
+        while (guess != secretNumber || guess != 5) {
+            System.out.println("\nYou are wrong.  Try again.\n\nEnter the number:  ");
+            guess = key.nextInt();
+
+        }
+            System.out.println("you are correct.  You win a prize!!");
+        }
+    }
